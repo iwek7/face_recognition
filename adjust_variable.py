158f2c8d13c6ef93159a3f8348db53b5f66f2250
iwek7 <miwanczuk7@gmail.com>
2016-07-24 18:15:00 +0200

    Przeniesienie obiektow z pliku neural_net_conv do wydzielonych plikow

---
+++ adjust_variable.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+
+# zmienia learning rate i momentum w czasie uczenia
+# nie uzywam, poprawa jest niewielka na moim zbiorze testowym
+
+class AdjustVariable(object):
+    def __init__(self, name, start=0.03, stop=0.001):
+        self.name = name
+        self.start, self.stop = start, stop
+        self.ls = None
+
+    def __call__(self, nn, train_history):
+        if self.ls is None:
+            self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
+
+        epoch = train_history[-1]['epoch']
+        new_value = float32(self.ls[epoch - 1])
+        getattr(nn, self.name).set_value(new_value)
